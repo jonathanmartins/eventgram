0d524bdf34645c9518ce8ecd06180b71c055c4be
Thiago de Lima Andrade <thiago@limaandrade.com>
2015-11-25 13:11:43 -0300

    Fix bugs on EventgramApplication.java

--- src/main/java/eventgram/EventgramApplication.java
+++ src/main/java/eventgram/EventgramApplication.java
@@ -23,18 +23,17 @@ public class EventgramApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception{
     	albumService.deleteAll();
     	
-    	Album mAlbum = new Album("Meu primeiro album", "This is a test");
-    	mAlbum.setCover("https://store-images.s-microsoft.com/image/apps."
-    			+ "12939.9007199266247104.04442c96-e9b7-4ee8-ac9a-ab3480429e8e.6856487f-cc4f-"
-    			+ "4d9d-b5f3-13eac205abd4?w=191&h=191");
+    	InstagramParser insta = new InstagramParser();
     	
+    	Album mAlbum = new Album("Meu primeiro album", "This is a test");
+    	mAlbum.setCover("https://store-images.s-microsoft.com/image/apps.12939.9007199266247104.04442c96-e9b7-4ee8-ac9a-ab3480429e8e.6856487f-cc4f-4d9d-b5f3-13eac205abd4?w=191&h=191");
+    	
+    	mAlbum.setPhotos(insta.parser("https://api.instagram.com/v1/tags/standuppaddle/media/recent?access_token=177062946.b9ea3ff.e2f796df3994408fbff60ef9e3e7ca37"));
     	albumService.create(mAlbum);
     	
     	Album mAlbum2 = new Album("Mais um album", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempus tempus velit eget pellentesque. "
     			+ "Curabitur sed enim massa. Vestibulum at dapibus felis. Etiam arcu tellus, ultrices non felis vel, efficitur dapibus tortor.");
-    	mAlbum2.setCover("https://store-images.s-microsoft.com/image/apps."
-    			+ "12939.9007199266247104.04442c96-e9b7-4ee8-ac9a-ab3480429e8e.6856487f-cc4f-"
-    			+ "4d9d-b5f3-13eac205abd4?w=191&h=191");
+    	mAlbum2.setCover("https://store-images.s-microsoft.com/image/apps.12939.9007199266247104.04442c96-e9b7-4ee8-ac9a-ab3480429e8e.6856487f-cc4f-4d9d-b5f3-13eac205abd4?w=191&h=191");
     	albumService.create(mAlbum2);
     	
     	System.out.println("Albums found with listAll");
@@ -49,7 +48,5 @@ public class EventgramApplication implements CommandLineRunner{
 		System.out.println("--------------------------------");
 		System.out.println(albumService.findByName("Meu primeiro album").getName());
 		
-		InstagramParser insta = new InstagramParser();
-		insta.parser("https://api.instagram.com/v1/tags/felicidade/media/recent?access_token=177062946.b9ea3ff.e2f796df3994408fbff60ef9e3e7ca37");
     }
 }
